4bac06ecd922857d32e35a53c11f393ca773bc8e
Alex <Alex@DESKTOP-VCD1O3N>
2018-10-30 21:00:27 +0800

    添加项目文件。

---
+++ GetResultFromTaskTResult/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetResultFromTaskTResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<object, long> del = (end) => 
+            {
+                long sum = 0;
+                for (int i = 0; i < (int)end; i++)
+                {
+                    sum += i;
+                }
+
+                return sum;
+            };
+
+            // Task<long> tsk = new Task<long>(del, 100000);
+            // tsk.Start();
+
+            Task<long> tsk = Task.Factory.StartNew(del, 100000);
+
+            Console.WriteLine("程序运行结果为{0}", tsk.Result); // tsk.Result 阻塞线程
+            Console.ReadKey();
+        }
+    }
+}
